a9ec47136b5669cf5640be55217c2b50d145a401
Stephaflex <stephaflex@rocketmail.com>
2022-11-15 11:17:07 -0800

    0x0D-preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,8 +2,7 @@
 #define OBJECT_LIKE_MACRO_H
 
 /**
- * File: 0-object_like_macro.h
- * Auth: Brennan D Baraban
+ * File: 0-object_like_macro
  * Desc: Header file that defines a macro named
  * SIZE as an abbreviation for the token 1024.
  */
